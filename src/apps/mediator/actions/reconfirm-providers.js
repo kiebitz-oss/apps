813719381d834067186331175985e5ae4c346630
Andreas Dewes <andreas.dewes@kiprotect.com>
2021-07-08 18:02:20 +0200

    Fixed 'reconfirmProviders' action.

--- src/apps/mediator/actions/reconfirm-providers.js
+++ src/apps/mediator/actions/reconfirm-providers.js
@@ -20,110 +20,116 @@ export async function reconfirmProviders(
         throw null; // we throw a null exception (which won't affect the store state)
     }
 
-    keyStore.set({ status: 'inProgress', i: 0, n: providers.length });
+    try {
+        keyStore.set({ status: 'inProgress', i: 0, n: providers.length });
 
-    let i = 0;
-    for (const providerData of providers) {
-        i++;
-        try {
-            const keyHashesData = {
-                signing: providerData.publicKeys.signing,
-                encryption: providerData.publicKeys.encryption,
-                queueData: {
+        let i = 0;
+        for (const providerData of providers) {
+            i++;
+            try {
+                const keyHashesData = {
+                    signing: providerData.publicKeys.signing,
+                    encryption: providerData.publicKeys.encryption,
+                    queueData: {
+                        zipCode: providerData.data.zipCode,
+                        accessible: providerData.data.accessible,
+                    },
+                    queues: providerData.data.queues, // so we know which queues the provider can query
+                };
+
+                const keysJSONData = JSON.stringify(keyHashesData);
+
+                // we remove the 'code' field from the provider
+                if (providerData.data.code !== undefined)
+                    delete providerData.data.code;
+
+                const publicProviderData = {
+                    name: providerData.data.name,
+                    street: providerData.data.street,
+                    city: providerData.data.city,
                     zipCode: providerData.data.zipCode,
-                    accessible: providerData.data.accessible,
-                },
-                queues: providerData.data.queues, // so we know which queues the provider can query
-            };
+                    website: providerData.data.website,
+                };
 
-            const keysJSONData = JSON.stringify(keyHashesData);
-
-            // we remove the 'code' field from the provider
-            if (providerData.data.code !== undefined)
-                delete providerData.data.code;
-
-            const publicProviderData = {
-                name: providerData.data.name,
-                street: providerData.data.street,
-                city: providerData.data.city,
-                zipCode: providerData.data.zipCode,
-                website: providerData.data.website,
-            };
-
-            const publicProviderJSONData = JSON.stringify(publicProviderData);
-            const providerJSONData = JSON.stringify(providerData.data);
-
-            const signedKeyData = await sign(
-                keyPairs.signing.privateKey,
-                keysJSONData,
-                keyPairs.signing.publicKey
-            );
-
-            const queues = await backend.appointments.getQueuesForProvider(
-                { queueIDs: providerData.data.queues },
-                keyPairs.signing
-            );
-
-            const queuePrivateKeys = [];
-            for (const queue of queues) {
-                const queuePrivateKey = await ecdhDecrypt(
-                    queue.encryptedPrivateKey,
-                    keyPairs.queue.privateKey
+                const publicProviderJSONData = JSON.stringify(
+                    publicProviderData
                 );
-                queuePrivateKeys.push({
-                    privateKey: queuePrivateKey,
-                    id: queue.id,
+                const providerJSONData = JSON.stringify(providerData.data);
+
+                const signedKeyData = await sign(
+                    keyPairs.signing.privateKey,
+                    keysJSONData,
+                    keyPairs.signing.publicKey
+                );
+
+                const queues = await backend.appointments.getQueuesForProvider(
+                    { queueIDs: providerData.data.queues },
+                    keyPairs.signing
+                );
+
+                const queuePrivateKeys = [];
+                for (const queue of queues) {
+                    const queuePrivateKey = await ecdhDecrypt(
+                        queue.encryptedPrivateKey,
+                        keyPairs.queue.privateKey
+                    );
+                    queuePrivateKeys.push({
+                        privateKey: queuePrivateKey,
+                        id: queue.id,
+                    });
+                }
+
+                // this will be stored for the provider, so we add the public key data
+                const signedProviderData = await sign(
+                    keyPairs.signing.privateKey,
+                    providerJSONData,
+                    keyPairs.signing.publicKey
+                );
+
+                const signedPublicProviderData = await sign(
+                    keyPairs.signing.privateKey,
+                    publicProviderJSONData,
+                    keyPairs.signing.publicKey
+                );
+
+                const fullData = {
+                    queuePrivateKeys: queuePrivateKeys,
+                    signedData: signedProviderData,
+                    signedPublicData: signedPublicProviderData,
+                };
+
+                // we encrypt the data with the public key supplied by the provider
+                const [encryptedProviderData, _] = await ephemeralECDHEncrypt(
+                    JSON.stringify(fullData),
+                    providerData.entry.encryptedData.publicKey
+                );
+
+                const result = await backend.appointments.confirmProvider(
+                    {
+                        id: providerData.id, // the ID of the unverified data
+                        verifiedID: providerData.verifiedID, // the ID to store the data under
+                        encryptedProviderData: encryptedProviderData,
+                        publicProviderData: signedPublicProviderData,
+                        signedKeyData: signedKeyData,
+                    },
+                    keyPairs.signing
+                );
+            } catch (e) {
+                console.error(e);
+            } finally {
+                keyStore.set({
+                    status: 'inProgress',
+                    i: i,
+                    n: providers.length,
                 });
             }
-
-            // this will be stored for the provider, so we add the public key data
-            const signedProviderData = await sign(
-                keyPairs.signing.privateKey,
-                providerJSONData,
-                keyPairs.signing.publicKey
-            );
-
-            const signedPublicProviderData = await sign(
-                keyPairs.signing.privateKey,
-                publicProviderJSONData,
-                keyPairs.signing.publicKey
-            );
-
-            const fullData = {
-                queuePrivateKeys: queuePrivateKeys,
-                signedData: signedProviderData,
-                signedPublicData: signedPublicProviderData,
-            };
-
-            // we encrypt the data with the public key supplied by the provider
-            const [encryptedProviderData, _] = await ephemeralECDHEncrypt(
-                JSON.stringify(fullData),
-                providerData.entry.encryptedData.publicKey
-            );
-
-            const result = await backend.appointments.confirmProvider(
-                {
-                    id: providerData.id, // the ID of the unverified data
-                    verifiedID: providerData.verifiedID, // the ID to store the data under
-                    encryptedProviderData: encryptedProviderData,
-                    publicProviderData: signedPublicProviderData,
-                    signedKeyData: signedKeyData,
-                },
-                keyPairs.signing
-            );
-
-            keyStore.set({
-                status: 'inProgress',
-                i: i,
-                n: providers.length,
-            });
-        } finally {
-            backend.local.unlock('reconfirmProviders');
         }
+        return {
+            status: 'succeeded',
+        };
+    } finally {
+        backend.local.unlock('reconfirmProviders');
     }
-    return {
-        status: 'succeeded',
-    };
 }
 
 reconfirmProviders.init = () => ({ status: 'initialized' });
